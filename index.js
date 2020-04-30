35026881bf7c5a073b8e7489cde1bc95359ffdf6
Evgeny Zislis <evgeny.zislis@gmail.com>
2020-04-30 22:43:50 +0000

    fix wsServer.emit() instead of ws.emit() and cleanup!

--- index.js
+++ index.js
@@ -3,14 +3,6 @@ log.error = log.extend('error')
 log.log = console.log.bind(console)
 log.error.log = console.error.bind(console)
 
-function json_filter(key, value) {
-  if (key.startsWith('_')) {
-    return undefined;
-  } else {
-    return value;
-  }
-}
-
 const http = require('http')
 const connect = require('connect')
 const WebSocket = require('ws')
@@ -30,24 +22,31 @@ const wsServer = new WebSocket.Server({
 
 server.on('upgrade', (req, socket, head) => {
   if (req.url.startsWith('/socket/')) {
-    wsServer.handleUpgrade(req, socket, head, ws => {
-      ws.emit('connection', ws, req)
+    return wsServer.handleUpgrade(req, socket, head, ws => {
+      wsServer.emit('connection', ws, req)
     })
-  } else {
-    log.error('Socket is using the wrong URL for WebSocket. Hanging up! (url:%o)', req.url)
-    socket.destroy()
   }
+  log.error('Request with wrong URL for WebSocket. Hanging up! %o',
+    { ip: req.remoteAddress, port: req.remotePort, url: req.url })
+  socket.destroy()
 })
 
-const responseTimeFn = (req, res, time) => {
-  log('request handling took %fms %s %s %o', parseFloat(time).toPrecision(3), req.method, req.url, { headers: req.headers })
-}
+/* record the time it took to respond to a request */
+app.use(responseTime(function (req, res, time) {
+  log('request handling took %fms %s %s %o : status %s',
+    parseFloat(time).toPrecision(3),
+    req.method,
+    req.url,
+    { headers: req.headers },
+    res.statusCode
+  )
+}))
 
-app.use(responseTime(responseTimeFn))
-
+/* handle webhooks & WebSub challenges */
 app.use('/hook/', function (req, res, next) {
   if (req.method === 'GET' && req.url.match(/hub.challenge=/)) {
     const challenge = req.url.replace(/.*hub.challenge=([^&]*).*$/, '$1')
+    log('Responding to challenge with: %o', { challenge })
     return res
       .writeHead(200, {
         'Content-Type': 'text/plain',
@@ -61,9 +60,7 @@ app.use('/hook/', function (req, res, next) {
   return next()
 })
 
-/**
- * "Catch All" route, for all the requests that did not match /hook/
- */
+/* "Catch All" route, for all the requests that did not match /hook/ */
 app.use(function (req, res, next) {
   const notFoundText = 'not found\n'
   return res
@@ -74,10 +71,6 @@ app.use(function (req, res, next) {
     .end(notFoundText)
 })
 
-server.listen({ host, port }, () => {
-  log(`Listening on http port ${port}.`)
-})
-
 server.on('connection', socket => {
   log(`new connection from %s:%s to %s:%s`,
     socket.remoteAddress, socket.remotePort,
@@ -85,31 +78,34 @@ server.on('connection', socket => {
   )
 })
 
-wsServer.on('connection', wsSocket => {
-  log('new websocket connection', JSON.stringify(wsSocket, json_filter))
-  wsSocket.isAlive = true
-  wsSocket.ping('PING', false, () => {
-    log('pinging', arguments)
-  })
-  wsSocket.on('pong', () => {
-    log('pong event triggered from %o', JSON.stringify(wsSocket, json_filter))
-    wsSocket.isAlive = true
-  })
-  wsSocket.on('error', (err) => {
-    console.error(`Client disconnected - reason: ${err}`);
+wsServer.on('connection', (ws, req) => {
+  ws.name = `${req.socket.remoteAddress}:${req.socket.remotePort}`
+  log('connection from %s upgraded to websocket', ws.name)
+  ws.isAlive = true
+  ws.ping(null, undefined)
+  ws.on('pong', () => { ws.isAlive = true })
+  ws.on('error', (err) => {
+    log.error('Client %s disconnected - reason: %o', ws.name, err);
   })
 })
 
-setInterval(() => {
-  wsServer.clients.forEach(wsSocket => {
-    if (!wsSocket.isAlive) {
-      log('ws is not alive, terminating', JSON.stringify(wsSocket, json_filter))
+const pingInterval = setInterval(() => {
+  wsServer.clients.forEach(ws => {
+    if (!ws.isAlive) {
+      log.error('WebSocket client %s is not alive, terminating!', ws.name)
       return wsSocket.terminate()
     }
-    wsSocket.isAlive = false
-    // wsSocket.send('PING')
-    wsSocket.ping('PING', false, () => {
-      log('pinging', arguments)
-    })
+    ws.isAlive = false
+    ws.ping(null, undefined)
   })
 }, 10000)
+
+wsServer.on('close', () => {
+  log('Closing WebSocket Server! %o', arguments)
+  clearInterval(pingInterval)
+})
+
+/* start the server */
+server.listen({ host, port }, () => {
+  console.log(`Listening on http port ${port}.`)
+})
